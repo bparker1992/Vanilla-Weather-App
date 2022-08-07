dd1fd35907146796d08864e80f4891f9caed03fd
bparker1992 <104226785+bparker1992@users.noreply.github.com>
2022-08-07 12:44:10 +0100

    Added Weather conversion

--- src/app.js
+++ src/app.js
@@ -33,7 +33,9 @@ function displayTemperature(response) {
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
 
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    celsiusTemperature = response.data.main.temp;
+
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
@@ -56,7 +58,32 @@ function handleSubmit(event) {
     search(cityInputElement.value)
 }
 
-search("London");
+function displayFarenheitTemperature(event) {
+    event.preventDefault();
+    let farenheitTemperature = (celsiusTemperature * 9 / 5) + 32;
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(farenheitTemperature);
+
+}
+
+
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+
+let celsiusTemperature = null;
+
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let farenheitLink = document.querySelector("#farenheit-link");
+farenheitLink.addEventListener("click", displayFarenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("London");
